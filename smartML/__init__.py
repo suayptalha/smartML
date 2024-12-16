b7ed0bc72fbb63b746ff539a965c475f3b3b2e51
Şuayp Talha Kocabay <162293855+suayptalha@users.noreply.github.com>
2024-12-16 19:51:50 +0300

    Update __init__.py

--- smartML/__init__.py
+++ smartML/__init__.py
@@ -1,2 +1,6 @@
-from smartML import *
-__all__ = ["algorithm", "dataset", "validation"]
+from smartML import algorithm
+from smartML import dataset
+from smartML import validation
+from smartML.algorithm import *
+from smartML.validation import *
+from smartML.dataset import *
