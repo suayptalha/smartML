81357bd3f03a9def0103e1a457d24ce6213bfe45
Şuayp Talha Kocabay <162293855+suayptalha@users.noreply.github.com>
2024-12-16 09:14:53 +0300

    Create algorithms.py

---
+++ algorithm/algorithms.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class KNearestNeighbors:
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        predictions = []
+        for x in X:
+            distances = np.sqrt(np.sum((self.X_train - x) ** 2, axis=1))
+            k_indices = np.argsort(distances)[:self.k]
+            k_labels = self.y_train[k_indices]
+            predictions.append(np.bincount(k_labels).argmax())
+        return predictions
